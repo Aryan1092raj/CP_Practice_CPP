e94c33397559e257734eb25be571fb55d404d824
Aryan1092raj <aryanraj1092@gmail.com>
2025-09-12 00:02:43 +0530

    gn

--- CP/loops.cpp
+++ CP/loops.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std ;
 int main(){
-    int n;
-    cin >> n ;
-    for(int i = 0; i<n; i++){
-        for(int j = 0 ; j<=i; j++){
-            cout<<"*";
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n ;
+        for(int i = 0; i<n; i++){
+            for(int j = 0 ; j<=i; j++){
+                cout<<"*";
+            }
+            cout<<endl;
         }
-        cout<<endl;
-
-
     }
-
 
     return 0 ;
 }
