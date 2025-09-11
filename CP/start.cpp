f8d3614c0ed363da96471987881e46f3527dbb99
Aryan1092raj <aryanraj1092@gmail.com>
2025-09-11 17:06:52 +0530

    hi

--- CP/start.cpp
+++ CP/start.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std ;
 int main(){
-    int a, b, c;
-    cin>>a >>b >>c;
+    long int a = 100000;
+    long int b = 100000;
+    long int c = a*b ;
+    cout<< c <<endl;
 
 }
